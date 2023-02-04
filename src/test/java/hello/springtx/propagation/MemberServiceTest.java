b4d5f4d24858511eb2e57478ab5e0246cdc6605c
zzlev <gpalektzja@gmail.com>
2023-02-04 17:36:39 +0900

    Add MemberServiceTest.

---
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,35 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired LogRepository logRepository;
+
+    /**
+     * memberService @Transactional: OFF
+     * logService @Transactional: ON
+     * logRepository @Transactional: ON
+     */
+    @Test
+    void outerTxOff_success() {
+        // given
+        String username = "outerTxOff_success";
+
+        // when
+        memberService.joinV1(username);
+
+        // then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+}
