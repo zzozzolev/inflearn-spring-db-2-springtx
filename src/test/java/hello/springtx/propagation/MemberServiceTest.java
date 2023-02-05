05eb3c6bfda8fd3b61be63d765965c9c4a7820b2
zzlev <gpalektzja@gmail.com>
2023-02-05 15:45:39 +0900

    Add recoverException_success test.

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -127,4 +127,23 @@ class MemberServiceTest {
         assertTrue(memberRepository.find(username).isEmpty());
         assertTrue(logRepository.find(username).isEmpty());
     }
+
+
+    /**
+     * memberService @Transactional: ON
+     * logService @Transactional: ON
+     * logRepository @Transactional: ON(REQUIRES_NEW) Exception
+     */
+    @Test
+    void recoverException_success() {
+        // given
+        String username = "로그예외_recoverException_success";
+
+        // when
+        memberService.joinV2(username);
+
+        // then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
 }
